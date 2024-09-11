c3000bbd462d131142a6781fdd79d1f6ad5dc73b
not a cat <32406395+astral-carrier@users.noreply.github.com>
2024-09-11 14:45:21 -0700

    q4

--- psets/pset1/q4.sql
+++ psets/pset1/q4.sql
@@ -1,2 +1,14 @@
 -- Q4
 -- Your query goes here.
+drop view if exists aggregates_by_route_view;
+drop view if exists route_data_view;
+
+create view aggregates_by_route_view as
+    select route_id, count(station_id) as total_stations, sum(distance_from_last_station_miles) as total_miles
+        from station_orders where distance_from_last_station_miles not null group by route_id;
+create view route_data_view as
+    select route_id, direction, route_name
+        from routes;
+
+select route_data_view.route_id, direction, route_name, total_stations, total_miles
+    from route_data_view join aggregates_by_route_view abrv on route_data_view.route_id = abrv.route_id;
