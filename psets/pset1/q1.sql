84fa15e34f7c1b58243ed5f0ec91167efd537146
not a cat <32406395+astral-carrier@users.noreply.github.com>
2024-09-10 21:48:23 -0700

    q1

--- psets/pset1/q1.sql
+++ psets/pset1/q1.sql
@@ -1,2 +1,3 @@
 -- Q1
 -- Your query goes here.
+select station_id, route_id, distance_from_last_station_miles from station_orders where distance_from_last_station_miles >= 1 order by distance_from_last_station_miles desc, route_id desc, station_id desc;
