7d7900119875be88b66e5844bba4b4aae00b1416
not a cat <32406395+astral-carrier@users.noreply.github.com>
2024-09-11 14:04:23 -0700

    q2

--- psets/pset1/q2.sql
+++ psets/pset1/q2.sql
@@ -1,2 +1,20 @@
 -- Q2
 -- Your query goes here.
+drop view if exists first_station_names_view;
+drop view if exists last_station_names_view;
+drop view if exists line_names_view;
+
+create view first_station_names_view as
+    select route_id, station_name as first_station_name
+        from routes join stations on first_station_id == station_id;
+create view last_station_names_view as
+    select route_id, station_name as last_station_name
+        from routes join stations on last_station_id == station_id;
+create view line_names_view as
+    select route_id, line_name
+        from routes join lines on routes.line_id == lines.line_id;
+
+select line_name, direction_desc, first_station_name, last_station_name
+    from routes join first_station_names_view fsn on routes.route_id = fsn.route_id
+    join last_station_names_view lsn on routes.route_id = lsn.route_id
+    join line_names_view lnv on routes.route_id = lnv.route_id;
