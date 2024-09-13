b94f894a5160ae8ea6a76478da87a46f56bd8b25
astral-carrier <32406395+astral-carrier@users.noreply.github.com>
2024-09-12 22:33:11 -0700

    q8-9

--- psets/pset1/q8.sql
+++ psets/pset1/q8.sql
@@ -1,2 +1,12 @@
 -- Q8
 -- Your query goes here.
+drop view if exists accepted_riderships_view;
+drop view if exists above_average_view;
+
+create view accepted_riderships_view as
+    select * from rail_ridership where time_period_id == 'time_period_01' and direction == 0 and season == 'Fall 2018';
+create view above_average_view as
+    select * from accepted_riderships_view where total_ons >= (select avg(total_ons) from accepted_riderships_view);
+
+select station_name, total_ons
+    from above_average_view join stations on above_average_view.station_id == stations.station_id;
--- psets/pset1/q9.sql
+++ psets/pset1/q9.sql
@@ -1,2 +1,17 @@
 -- Q9
 -- Your query goes here.
+drop view if exists station_route_counts_view;
+drop view if exists max_routes_station_view;
+drop view if exists max_station_routes_view;
+
+create view station_route_counts_view as
+    select *, count(route_id) as route_count from station_orders group by station_id;
+create view max_routes_station_view as
+    select station_id, route_count from station_route_counts_view where route_count == (select max(route_count) from station_route_counts_view);
+create view max_station_routes_view as
+    select station_orders.station_id, route_id, route_count
+        from station_orders join max_routes_station_view on station_orders.station_id == max_routes_station_view.station_id;
+
+select station_name, routes.route_id, line_id, route_count
+    from max_station_routes_view join stations on stations.station_id == max_station_routes_view.station_id
+    join routes on max_station_routes_view.route_id == routes.route_id;
