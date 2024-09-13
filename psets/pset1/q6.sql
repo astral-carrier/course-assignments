f04b26272ba7df6a8f6e6db70c6a1874a4084be8
astral-carrier <32406395+astral-carrier@users.noreply.github.com>
2024-09-12 23:16:37 -0700

    possible q6 fix?

--- psets/pset1/q6.sql
+++ psets/pset1/q6.sql
@@ -1,12 +1,12 @@
 -- Q6
 -- Your query goes here.
 drop view if exists summer_entries_view;
-drop view if exists max_entries;
 
 create view summer_entries_view as
-    select station_name, gated_entries from gated_station_entries join
-        stations on stations.station_id == gated_station_entries.station_id and service_date regexp '^2021-0[678]-\d\d$';
-create view max_entries as
-    select max(gated_entries) from summer_entries_view;
+    select station_name, sum(gated_entries) as gated_entries_sum
+        from gated_station_entries join stations on stations.station_id == gated_station_entries.station_id and
+                                                    service_date regexp '^2021-0[678]-\d\d$'
+        group by station_name;
 
-select station_name, gated_entries from summer_entries_view where gated_entries == (select * from max_entries);
+select station_name, gated_entries_sum
+    from summer_entries_view where gated_entries_sum == (select max(gated_entries_sum) from summer_entries_view);
