d21dc5e8efe2b0687b807410077d6fba2fba1167
astral-carrier <32406395+astral-carrier@users.noreply.github.com>
2024-09-12 23:04:36 -0700

    [fix] asc for some of q1

--- psets/pset1/q1.sql
+++ psets/pset1/q1.sql
@@ -1,3 +1,3 @@
 -- Q1
 -- Your query goes here.
-select station_id, route_id, distance_from_last_station_miles from station_orders where distance_from_last_station_miles >= 1 order by distance_from_last_station_miles desc, route_id desc, station_id desc;
+select station_id, route_id, distance_from_last_station_miles from station_orders where distance_from_last_station_miles >= 1 order by distance_from_last_station_miles desc, route_id, station_id;
